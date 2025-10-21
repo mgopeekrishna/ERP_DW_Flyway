718704df7b9a354855b0e3eda10d630d1e52f41e
Gopi Mucharla <gopikrishna.mucharla@gmail.com>
2025-10-20 23:05:58 -0700

    Initial commit with Flyway V1 and V2 migrations

---
+++ flyway/sql/V1__init_schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE dbo.sample_table (
+    id INT PRIMARY KEY,
+    name NVARCHAR(100) NOT NULL
+);
---
+++ flyway/sql/V2__add_sample_data.sql
@@ -0,0 +1,2 @@
+-- V2__add_sample_data.sql
+INSERT INTO dbo.sample_table (id, name) VALUES (1, N'Alpha'), (2, N'Beta');
